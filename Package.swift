5fa6bff7ba5ca841f14fe15045d8d048d1bd978d
Gwen Developer <msproductivity2007@gmail.com>
2026-08-11 19:47:31 +0530

    feat(project): initialize project structure, gitignore, and swift package manifest

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "Gwen",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .executable(name: "Gwen", targets: ["Gwen"])
+    ],
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "Gwen",
+            dependencies: [],
+            path: "Sources/Gwen",
+            swiftSettings: []
+        )
+    ]
+)
